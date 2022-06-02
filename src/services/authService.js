6f8d67da0eb49be9c01dd3f379cccde83b25f6b9
Augusto Daniel <augusto.danieldesouza.ext@bayer.com>
2022-06-02 02:04:43 -0300

    fix: Adjust in router update roles

--- src/services/authService.js
+++ src/services/authService.js
@@ -71,7 +71,7 @@ exports.createRole = async (req, res) => {
     const newRole = new Role(req.body);
 
     newRole.save((err, role) => {
-      if (err) return res.status(500).send({ message: MSG.INTERNAL_ERROR });
+      if (err) return res.status(500).send({ message: MSG.ROLE_FAIL });
       res.send({ message: MSG.ROLE_SUCESS, role: role });
     });
   });
@@ -82,10 +82,15 @@ exports.updateRole = async (req, res) => {
     const filter = { name: req.params.name };
     const update = { name: req.body.name };
     const options = { new: true };
-    await Role.updateOne(filter, update, options, (err, role) => {
-      if (err) return res.status(500).send({ message: err });
-      if (role.modifiedCount === 0) return res.status(400).send({ message: "Role not found!" });
-      if (role.modifiedCount === 1) return res.status(200).send({ message: "Role was updated successfully!", role: update });
+    Role.findOne({ name: req.params.name }, (err, rol) => {
+      if (err) return res.status(500).send({ message: MSG.INTERNAL_ERROR });
+      if (!rol) return res.status(400).send({ message: MSG.ROLE_NOT_FOUND });
+
+      Role.updateOne(filter, update, options, (err, role) => {
+        if (err) return res.status(500).send({ message: MSG.ROLE_UPDATE_FAIL });
+        if (role.modifiedCount === 0) return res.status(400).send({ message: MSG.ROLE_NOT_FOUND });
+        if (role.modifiedCount === 1) return res.status(200).send({ message: MSG.ROLE_UPDATE_SUCESS, role: update });
+      });
     });
   } catch (error) {
 
@@ -110,7 +115,7 @@ exports.getRoles = async (req, res) => {
     await Role.find((err, roles) => {
       if (err) return res.status(500).send({ message: err });
       if (roles.length === 0) return res.status(400).send({ message: "Roles not found!" });
-      return res.status(200).send({ message: "Roles found!", roles });
+      return res.status(200).send({ message: "Success", roles });
     });
   } catch (error) {
 
