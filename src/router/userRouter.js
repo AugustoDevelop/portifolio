78e8bededf392a5fd1edadc2a27add1d161939b4
Augusto Daniel <augusto.danieldesouza.ext@bayer.com>
2022-06-12 02:53:32 -0300

    feature: Create Doc route Delete, clean code and adjust msg

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -2,16 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
 const authencation = require("../middleware/authJwt");
-const { check, body } = require('express-validator');
+const { check } = require('express-validator');
 
 router.get('/', authencation.verifyToken, userController.users);
-
-router.get('/:email', [
-  check('email').isEmail(),
-  authencation.verifyToken
-], userController.user);
-
+router.get('/:email', [check('email').isEmail(), authencation.verifyToken], userController.user);
 router.patch('/:email', authencation.verifyToken, userController.updateUser);
-
 
 module.exports = router;
